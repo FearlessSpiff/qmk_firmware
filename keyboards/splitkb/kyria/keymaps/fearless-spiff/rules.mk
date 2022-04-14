0c6ad9736fc6b44f92ce1586910b22310a74fca3
FearlessSpiff <fearless.spiff@me.com>
2022-04-14 08:12:30 +0200

    add FearlessSpiff Kyria keymap

---
+++ keyboards/splitkb/kyria/keymaps/fearless-spiff/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+VIA_ENABLE = no
+
+MOUSEKEY_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
+
